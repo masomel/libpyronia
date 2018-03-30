9cf9b2ba4eb81c456ed3d12b7e64170d05d0d861
Marcela S. Melara <msmelara@gmail.com>
2018-03-30 16:24:38 -0700

    Run a separate SI thread, add AA profile for test

--- libraries/libpyronia/testsuite/kernel_upcall_test.c
+++ libraries/libpyronia/testsuite/kernel_upcall_test.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <pyronia_lib.h>
+#include <error.h>
+#include <errno.h>
+#include <pthread.h>
+
+void *wait_for_kernel_reqs(void *args){
+    pyr_recv_from_kernel();
+    return NULL;
+}
+
+static int test_file_open() {
+    printf("-- Test: authorized file open for reading... ");
+    FILE *f;
+    f = fopen("/tmp/cam0", "r");
+
+    if (f == NULL) {
+        printf("%s\n", strerror(errno));
+        return -1;
+    }
+
+    printf("success\n");
+    fclose(f);
+    return 0;
+}
 
 int main (int argc, char *argv[]) {
   int ret = 0;
-  
+  pthread_t tid;
+
   printf("Initializing Pyronia userland extensions\n");
-  
+
   ret = pyr_init();
 
   if (ret) {
@@ -14,9 +38,11 @@ int main (int argc, char *argv[]) {
     goto out;
   }
 
-  pyr_recv_from_kernel();
-  
+  pthread_create(&tid, NULL, wait_for_kernel_reqs, NULL);
+
+  test_file_open();
  out:
-  //pyr_exit();
+  pthread_join(tid, NULL);
+  pyr_exit();
   return ret;
 }
