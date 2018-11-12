b72c96c5bf1e84ae828866aef58f16a95d66fbfe
Marcela S. Melara <msmelara@gmail.com>
2018-11-11 23:33:48 -0800

    Add support for function sandboxes and data object policy parsing

--- src/serialization.c
+++ src/serialization.c
@@ -43,14 +43,14 @@ int pyr_serialize_callstack(char **cs_str, pyr_cg_node_t *callstack) {
         if (!tmp_ser)
             goto fail;
 
-	// UGH need to clear the very first allocation,
-	// so we don't accidentally start concatenating to
-	// junk that realloc spits out
-	if (ser_len == 1) {
-	  memset(tmp_ser, 0, ser_len+strlen(cur_node->lib)+1);
-	}
+        // UGH need to clear the very first allocation,
+        // so we don't accidentally start concatenating to
+        // junk that realloc spits out
+        if (ser_len == 1) {
+          memset(tmp_ser, 0, ser_len+strlen(cur_node->lib)+1);
+        }
 
-	ser = tmp_ser;
+        ser = tmp_ser;
 
         strncat(ser, cur_node->lib, strlen(cur_node->lib));
         strncat(ser, CALLSTACK_STR_DELIM, 1);
@@ -58,7 +58,7 @@ int pyr_serialize_callstack(char **cs_str, pyr_cg_node_t *callstack) {
         cur_node = cur_node->child;
         node_count++;
 
-	printf("[%s] Serialized node: %s, # nodes %d\n", __func__, ser, node_count);
+        printf("[%s] Serialized node: %s, # nodes %d\n", __func__, ser, node_count);
     }
 
     // now we need to pre-append the len so the kernel knows how many
@@ -91,7 +91,7 @@ static int read_policy_file(const char *policy_fname, char **buf) {
         fseek(f, 0, SEEK_SET);
         buffer = pyr_alloc_critical_runtime_state(length+1);
         if (!buffer) {
-	  printf("[%s] Could not allocate the protected buffer\n", __func__);
+          printf("[%s] Could not allocate the protected buffer\n", __func__);
             goto fail;
         }
         read = fread(buffer, 1, length, f);
@@ -99,9 +99,9 @@ static int read_policy_file(const char *policy_fname, char **buf) {
           printf("[%s] Bad read length: %d != %d\n", __func__, read, length);
           goto fail;
         }
-	buffer[length] = '\0';
+        buffer[length] = '\0';
         *buf = buffer;
-	fclose(f);
+        fclose(f);
         return length;
     }
     else {
@@ -116,15 +116,20 @@ static int read_policy_file(const char *policy_fname, char **buf) {
     return -1;
 }
 
-/* Reads the library policy at in the poliy file at policy_fname,
- * and serializes it for registration with the LSM.
+/* Reads the library policy (OS resources and data objects)
+ * in the policy file at policy_fname,
+ * and serializes it for registration with the LSM and the runtime.
  */
-int pyr_parse_lib_policy(const char *policy_fname, char **parsed) {
+int pyr_parse_lib_policy(const char *policy_fname, char **parsed,
+                         char **parsed_obj_rules, int *num_rules) {
 
     int rule_len;
     char *ser = NULL, *out = NULL, *tmp_ser = NULL;
     uint32_t count = 0, ser_len = 1; // for null-byte
     int ret;
+    char *obj_rules[1024];
+    int obj_rule_count = 0;
+    char **out_obj_rules = NULL;
 
     char *policy;
     char *policyp;
@@ -133,37 +138,45 @@ int pyr_parse_lib_policy(const char *policy_fname, char **parsed) {
         goto fail;
     }
     policyp = policy;
-    
+
     // loop through the policy to serialize it into
     // a format that can be interpreted by the LSM
     char *next_rule = strsep(&policy, "\n");
     while(next_rule) {
         if (!strlen(next_rule)) {
-	  // this means we've hit an empty line, so skip to next rule
-	  next_rule = strsep(&policy, "\n");
-	  continue;
-	}
-      
-	rule_len = strlen(next_rule);
-	tmp_ser = realloc(ser, ser_len+rule_len);
-	if (!tmp_ser) {
-	  ret = -1;
-	  goto fail;
-	}
-	// UGH need to clear the very first allocation,
-	// so we don't accidentally start concatenating to
-	// junk that realloc spits out
-	if (ser_len == 1) {
-	  memset(tmp_ser, 0, ser_len+rule_len);
-	}
-	ser = tmp_ser;
-	
-	strncat(ser, next_rule, rule_len);
-	ser_len = strlen(ser)+rule_len;
-	count++;
-	next_rule = strsep(&policy, "\n");
+          // this means we've hit an empty line, so skip to next rule
+          next_rule = strsep(&policy, "\n");
+          continue;
+        }
+
+        rule_len = strlen(next_rule);
+        tmp_ser = realloc(ser, ser_len+rule_len);
+        if (!tmp_ser) {
+          ret = -1;
+          goto fail;
+        }
+        // UGH need to clear the very first allocation,
+        // so we don't accidentally start concatenating to
+        // junk that realloc spits out
+        if (ser_len == 1) {
+          memset(tmp_ser, 0, ser_len+rule_len);
+        }
+        ser = tmp_ser;
+
+        // this is a data object rule, so keep is aside, and
+        // deal with it later
+        if (ser[0] == '-' || ser[0] == '+') {
+            obj_rules[obj_rule_count] = ser;
+            obj_rule_count++;
+        }
+        else {
+            strncat(ser, next_rule, rule_len);
+            ser_len = strlen(ser)+rule_len;
+            count++;
+        }
+        next_rule = strsep(&policy, "\n");
     }
-    
+
     if (count == 0) {
       // this means our file is malformed
       // bc we don't have a single valid rule line
@@ -181,16 +194,36 @@ int pyr_parse_lib_policy(const char *policy_fname, char **parsed) {
     }
     memset(out, 0, strlen(ser)+INT32_STR_SIZE+2);
     ret = sprintf(out, "%d,%s", count, ser);
+
+    if (obj_rule_count) {
+        int i = 0;
+        out_obj_rules = malloc(sizeof(char *)*obj_rule_count);
+        if (!out_obj_rules) {
+            ret = -1;
+            goto fail;
+        }
+        for (i = 0; i < obj_rule_count; i++) {
+            out_obj_rules[i] = malloc(strlen(obj_rules[i])+1);
+            if (!out_obj_rules[i]) {
+                ret = -1;
+                goto fail;
+            }
+            memset(out_obj_rules[i], 0, strlen(obj_rules[i])+1);
+            memcpy(out_obj_rules[i], obj_rules[i], strlen(obj_rules[i]));
+        }
+        *num_rules = obj_rule_count;
+    }
     goto done;
-    
+
  fail:
     out = NULL;
-    
+    out_obj_rules = NULL;
  done:
     if (policyp)
       pyr_free_critical_state(policyp);
     if (ser)
         free(ser);
     *parsed = out;
+    parsed_obj_rules = out_obj_rules;
     return ret;
 }
--- src/serialization.h
+++ src/serialization.h
@@ -8,15 +8,22 @@
 #define PYR_SERIALIZE_H
 
 #include <linux/pyronia_mac.h>
+#include "data_obj.h"
 
 #define INT32_STR_SIZE 12
+#define RO_DATA_OBJ_MARKER "-"
+#define RW_DATA_OBJ_MARKER "+"
+#define DOMAIN_DELIM ":"
+#define FUNC_NAME_DELIM " "
+#define LIB_RULE_DELIM ","
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int pyr_serialize_callstack(char **cs_str, pyr_cg_node_t *callstack);
-int pyr_parse_lib_policy(const char *policy_fname, char **parsed);
+    int pyr_serialize_callstack(char **cs_str, pyr_cg_node_t *callstack);
+    int pyr_parse_lib_policy(const char *policy_fname, char **parsed,
+                             char **parsed_obj_rules, int *num_rules);
 
 #ifdef __cplusplus
 }
